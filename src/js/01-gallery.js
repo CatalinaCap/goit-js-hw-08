a19eeab60694f320444fdabb733d5f9b39a014de
BiancaLZC <160909595+BiancaLZC@users.noreply.github.com>
2024-08-10 10:30:01 +0200

    hw done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+galleryItems.forEach(e => {
+  const { preview, original, description } = e;
+  const img = `<li class='gallery__item'>
+          <a class='gallery__link' href='${original}'>
+              <img src='${preview}' alt='${description}' class='gallery__image' data-source='${original}' title='${description}'>
+          </a>
+      </li>`;
+  gallery.insertAdjacentHTML('beforeend', img);
+});
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('closed.simplelightbox', () => {
+    gallery.refresh();
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+lightbox.on("show.simplelightbox", function () {
+  setTimeout(applyImageStyles, 100);
+});
+lightbox.on("change.simplelightbox", function () {
+  applyImageStyles();
+});
